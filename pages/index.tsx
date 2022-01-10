992468936fa7443078b4de929b11678f0c95dc47
Jack Allen <jackallensemail+git@gmail.com>
2022-01-10 18:51:36 +0000

    stuff probably

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Home() {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    push('/accommodation').then();
+  }, []);
+
+  return (
+    <>
+
+    </>
+  )
+}
