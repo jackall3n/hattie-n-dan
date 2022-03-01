458f74fb8bd087eb10e88499d1c9936d9c69c133
Dan Williams <daniel.williams@inshur.com>
2022-03-01 21:32:17 +0000

    fix mailto formatting

--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -28,10 +28,10 @@ export default function RSVP() {
         <div className="flex">
           <div className="m-auto">
             <a
-              href="mailto:dattiewedding@gmail.com?subject=RSVP: Dan and Hattie wedding&body=Name:%0D%0AI+can+attend+the+Friday+evening:%0D%0AI+can+attend+the+Saturday+swim (please specify if you need a wetsuit):%0D%0AI+can+attend+the+wedding:%0D%0ADietary requirements:"
+              href="mailto:dattiewedding@gmail.com?bcc=harrietsime@gmail.com&subject=RSVP%3A%20Dan%20and%20Hattie%20wedding&body=Name%3A%0AI%20can%20attend%20the%20Friday%20evening%3A%0AI%20can%20attend%20the%20Saturday%20swim%20(please%20specify%20if%20you%20need%20a%20wetsuit)%3A%0AI%20can%20attend%20the%20wedding%3A%0ADietary%20requirements%3A"
               className="mt-10 block bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
             >
-              RSVP
+              RSVP Here
             </a>
           </div>
         </div>
